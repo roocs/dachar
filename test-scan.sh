99b40c2366b6a036d2030f0bd3b7b7b9290b554c
Eleanor Smith <esmith88@jasmin-sci4.ceda.ac.uk>
2020-04-02 15:33:03 +0100

    small changes to run tests

--- test-scan.sh
+++ test-scan.sh
@@ -9,8 +9,8 @@
 # facets="activity product institute model experiment frequency realm mip_table ensemble_member version variable"
 # /badc/cmip5/data/cmip5/output1/*/*/rcp45/mon/ocean/Omon/r1i1p1/latest/zostoga/*
 
-#facets="activity=cmip5,product=output1,experiment=rcp45,frequency=mon,realm=ocean,mip_table=Omon,ensemble_member=r1i1p1,version=latest,variable=zostoga"
-#python scan.py -l ceda -f $facets -m quick cmip5
+facets="activity=cmip5,product=output1,experiment=rcp45,frequency=mon,realm=ocean,mip_table=Omon,ensemble_member=r1i1p1,version=latest,variable=zostoga"
+dachar scan -l ceda -f $facets -m quick cmip5
 
 dachar scan -l ceda -d cmip5.output1.MRI.MRI-CGCM3.rcp45.mon.ocean.Omon.r1i1p1.latest.zostoga -m full cmip5
 
