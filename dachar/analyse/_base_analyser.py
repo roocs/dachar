27d77d993f9f9474911ca404672f41636e5f5083
ellesmith88 <e.s.smith@hotmail.co.uk>
2020-05-04 10:38:18 +0100

    analysis workflow

--- dachar/analyse/_base_analyser.py
+++ dachar/analyse/_base_analyser.py
@@ -1,14 +1,16 @@
 from dachar.utils import UNDEFINED
 from dachar.fixes._base_fix import FixDetails
 from dachar import dc_store, fix_proposal_store, ar_store
-from dachar.fixes.fix_api import get_fix_categories, get_fix_dict
 from dachar.utils.options import get_checks
 from dachar.analyse.checks.coord_checks import RankCheck
 from dachar.utils import options
+from dachar import __version__ as version
 
-import fnmatch
+
 import os
+import datetime
 import glob
+from pydoc import locate
 
 """
 1. Per project, can we run a massive "analyse all" job.
@@ -48,12 +50,23 @@ class AnalysisReport(object):
     Provides structure for analysis report object
     """
 
-    def __init__(self):
+    def __init__(self, sample_id, ds_ids, checks):
         self.data = {}
+        self.record = {
+            'sample_id': sample_id,
+            'dataset_ids': ds_ids,
+            'checks': checks,
+            'proposed_fixes': [],
+            'analysis_metadata': {
+                'location': 'ceda',
+                'datetime': datetime.datetime.now(),
+                'software_version': version
+            }
+        }
 
-    @property
-    def proposed_fixes(self):
-        return self.data['proposed_fixes']
+    # @property
+    # def proposed_fixes(self):
+    #     return self.data['proposed_fixes']
 
     def add_fix(self, fix):
         fixes = self.data.get('proposed_fixes', [])
@@ -61,33 +74,33 @@ class AnalysisReport(object):
         self.data['proposed_fixes'] = fixes
 
 
-class FixProposal(object):
-    """
-    structure of record
-        {'dataset_id': 'ds.1.1.1.1.1.1',
-         'fixes': [{'fix': {'fix_id': 'fix_id',
-                            'title': 'title',
-                            'description': 'description',
-                            'category': 'category',
-                            'reference_implementation': 'ref_implementation',
-                            'operands': 'operands'},
-                    'history': [],
-                    'reason': '',
-                    'status': 'proposed',
-                    'timestamp': '2020-04-29T14:41:52'}]}
-    """
-
-    def __init__(self):
-        self.data = {}
-
-    @property
-    def fixes(self):
-        return self.data['fixes']
-
-    def add_fix(self, fix):
-        fixes = self.data.get('fixes', [])
-        fixes.append(fix)
-        self.data['fixes'] = fixes
+# class FixProposal(object):
+#     """
+#     structure of record
+#         {'dataset_id': 'ds.1.1.1.1.1.1',
+#          'fixes': [{'fix': {'fix_id': 'fix_id',
+#                             'title': 'title',
+#                             'description': 'description',
+#                             'category': 'category',
+#                             'reference_implementation': 'ref_implementation',
+#                             'operands': 'operands'},
+#                     'history': [],
+#                     'reason': '',
+#                     'status': 'proposed',
+#                     'timestamp': '2020-04-29T14:41:52'}]}
+#     """
+#
+#     def __init__(self):
+#         self.data = {}
+#
+#     @property
+#     def fixes(self):
+#         return self.data['fixes']
+#
+#     def add_fix(self, fix):
+#         fixes = self.data.get('fixes', [])
+#         fixes.append(fix)
+#         self.data['fixes'] = fixes
 
 
 class OneSampleAnalyser(object):
@@ -104,33 +117,22 @@ class OneSampleAnalyser(object):
         self.project = project
         self.force = force
 
-
     def _load_ids(self, sample_id):
         """ Gets list of possible ds_ids from sample_id"""
-        #not actually sure how to do this
-        # don't think it will work
+
         base_dir = options.project_base_dirs[self.project]
         sample_id = os.path.join(base_dir, '/'.join(sample_id.split('.')))
 
-
         sample = []
         for path in glob.glob(sample_id):
-            sample.append(path.split('/')[2:].replace('.', '/'))
-
-        # should be return sample
-
-        sample = ['cmip5.output1.MOHC.HadGEM2-ES.rcp45.mon.ocean.Omon.r1i1p1.latest.zostoga',
-                  'cmip5.output1.MOHC.HadGEM2-CC.rcp45.mon.ocean.Omon.r1i1p1.latest.zostoga',]
+            sample.append('.'.join(path.split('/')[4:]))
 
         return sample
-        # use json store to load into a list of dicts
 
     def _characterised(self):
         """ Checks whether ds_ids in sample have been characterised.
         Gets character files from the store or raises and exception
         if some haven't been characterised"""
-        #need to check if length of sample is greater than one - maybe ??
-
 
         sample = self._load_ids(self)
         self._cache = {}
@@ -147,14 +149,12 @@ class OneSampleAnalyser(object):
 
     def _analysed(self):
         """
-        checks whether already analysed if not - if analysed and
-        force =False then stop.
-        If analysed and force = True then carry on
-        print statements to say what is happening
+        checks whether already analysed if not - if analysed and force =False then stop.
+        If analysed and force = True then carry on. Print statements to say what is happening
         :return:
         """
         if ar_store.exists(self.sample_id) and self.force is True:
-            print()
+            print(f'Overwriting existing analuysis for {self.sample_id}.')
         elif ar_store.exists(self.sample_id) and self.force is False:
             raise Exception(f'Analysis already run for {self.sample_id}. '
                             f'Use force=True to overwrite.')
@@ -166,13 +166,11 @@ class OneSampleAnalyser(object):
 
         # needs to keep track of fix proposal and return
         # need to create check object using name of check
-        check = RankCheck(sample)
-        content = check._extract_content()
+        check = check(sample)
         run = check.run()
         if run is not None:
-            dict = check.deduce_fix(run)
-            return dict
-
+            d = check.deduce_fix(run)
+            return d
 
     def analyse(self):
         """
@@ -180,9 +178,6 @@ class OneSampleAnalyser(object):
         saves all results in proposed fixes store(?)
         :return:
         """
-        # Create analysis record
-        arecord = AnalysisReport() # need to add stuff to this in this function
-
         # check if analysed and characterised
         self._characterised()
         self._analysed()
@@ -191,19 +186,21 @@ class OneSampleAnalyser(object):
         sample = self._cache
         checks = get_checks(self.project)
 
+        # Create analysis record
+        a_record = AnalysisReport(self.sample_id, sample, checks)
+
         results = {}
 
         for check in checks:
-            results[check] = self.run_check(check, sample)
+            check_cls = locate(f'dachar.analyse.checks.{check}')
+            results[check] = self.run_check(check_cls, sample)
 
-        for check in results:  #need to make use of fixproposal class
-            data = results.get(check)
-            ds_id = data['ds_id']
-            fix = data['fix']
-            check._propose_fix(ds_id, fix) # this sends fixes to fix_proposal_store
+        for check in results:
+            fix_dict = results.get(check)
+            a_record.add_fix(fix_dict)
+            fix_proposal_store.propose(fix_dict['dataset_id'], fix_dict['fix'])
 
-        #fix_proposal_store
-        ar_store.put(self.sample_id, arecord, force=self.force)
+        ar_store.put(self.sample_id, a_record, force=self.force)
 
         # @property
         # def results(self):
@@ -235,15 +232,8 @@ class GrandAnalyser(object):
 
 if __name__ == '__main__':
     zostoga_sample_id = "cmip5.output1.*.*.rcp45.mon.ocean.Omon.r1i1p1.latest.zostoga"
-    base_dir = options.project_base_dirs['cmip5']
-    sample_id = os.path.join(base_dir, '/'.join(zostoga_sample_id.split('.')))
-    print(sample_id)
-    sample = []
-    for path in glob.glob(sample_id):
-        sample.append(path.split('/')[2:].replace('.', '/'))
-    print(sample)
-    #zostoga = OneSampleAnalyser(zostoga_sample_id, 'cmip5', force=False)
-    #zostoga.analyse()
+    zostoga = OneSampleAnalyser(zostoga_sample_id, 'cmip5', force=False)
+    zostoga.analyse()
     # Try and get onesampleanalyser working for zostoga example - only check with
     # squeezedims
     # Needs to produce analysis record
