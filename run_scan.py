89582da662f5d59cccad70c0f4a9f5a26a76eea1
ellesmith88 <e.s.smith@hotmail.co.uk>
2020-05-26 15:54:29 +0100

    scripts to scan all c3s-cmip5 datasets

--- run_scan.py
+++ run_scan.py
@@ -1,3 +1,35 @@
-def convert_to_ds_id():
+from dachar.utils import switch_ds
+import argparse
+import glob
+import subprocess
 
-def run_scan():
+
+def arg_parse():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-p', '--path', type=str, required=True)
+
+    return parser.parse_args()
+
+
+def convert_to_ds_id_and_scan(args):
+    project = 'c3s-cmip5'
+    ensemble_path = args.path
+    vars = glob.glob(f'{ensemble_path}/*')
+
+    for var_path in vars:
+        ds_id = switch_ds.switch_ds(project, var_path)
+        print(ds_id)
+        cmd = f'dachar scan -l ceda -d {ds_id} -m full {project}'
+        # subprocess.call(cmd, shell=True)
+
+
+def main():
+
+    args = arg_parse()
+    convert_to_ds_id_and_scan(args)
+
+
+if __name__ == '__main__':
+    main()
+
--- scan_all.py
+++ scan_all.py
@@ -1,8 +1,10 @@
 # scan all c3s-cmip5 datasets using lotus
-
+from dachar import config
 import glob
+import os
+import subprocess
 
-glob.glob('/group_workspaces/jasmin2/cp4cds1/vol1/data/c3s-cmip5/output1/*/*/*/*/*/*/*/*')
+# glob.glob('/group_workspaces/jasmin2/cp4cds1/vol1/data/c3s-cmip5/output1/*/*/*/*/*/*/*/*')
 
 
 def get_institute_model_combination():
@@ -26,14 +28,25 @@ def get_ensemble(freq_path):
 
     for ensemble_path in ensembles:
 
-
-
-output_base = f"{lotus_output_path}/{ensemble}"
+        current_directory = os.getcwd()
+        output_base = config.BATCH_OUTPUT_PATH # need to figure out how log paths works
 
         # submit to lotus
-        bsub_command = f"bsub -q {SETTINGS.QUEUE} -W {SETTINGS.WALLCLOCK} -o " \
-                       f"{output_base}.out -e {output_base}.err {current_directory}" \
-                       f"/run_chunk.py -s {stat} -m {model} -e {ensemble} -v {variables}"
+        # bsub_command = f"bsub -q {config.QUEUE} -W {config.WALLCLOCK} -o " \
+        #                f"{output_base}.out -e {output_base}.err {current_directory}" \
+        #                f"/run_scan.py -p {ensemble_path}"
+
+        bsub_command = f'run_scan.py -p {ensemble_path}'
         subprocess.call(bsub_command, shell=True)
 
         print(f"running {bsub_command}")
+
+
+def main():
+    get_institute_model_combination()
+
+
+if __name__ == '__main__':
+    main()
+
+
