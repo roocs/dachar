ec9a15bba77f78a1a5201f04f30ff04396bdc2a0
ellesmith88 <e.s.smith@hotmail.co.uk>
2020-05-28 15:11:51 +0100

    black changes

--- dachar/utils/options.py
+++ dachar/utils/options.py
@@ -1,26 +1,22 @@
 project_base_dirs = {
-    'cmip5': '/badc/cmip5/data',
-    'cmip6': '/badc/cmip6/data',
-    'cordex': '/badc/cordex/data',
-    'c3s-cmip5': '/group_workspaces/jasmin2/cp4cds1/data',
-    'c3s-cmip6': 'NOT DEFINED YET',
-    'c3s-cordex': '/group_workspaces/jasmin2/cp4cds1/data',
+    "cmip5": "/badc/cmip5/data",
+    "cmip6": "/badc/cmip6/data",
+    "cordex": "/badc/cordex/data",
+    "c3s-cmip5": "/group_workspaces/jasmin2/cp4cds1/data",
+    "c3s-cmip6": "NOT DEFINED YET",
+    "c3s-cordex": "/group_workspaces/jasmin2/cp4cds1/data",
 }
 known_projects = project_base_dirs.keys()
-locations = ['ceda', 'dkrz', 'other']
+locations = ["ceda", "dkrz", "other"]
 facet_rules = {
-    'cmip5': 'activity product institute model experiment frequency realm mip_table ensemble_member version variable'.split(),
-    'cmip6': 'mip_era activity_id institution_id source_id experiment_id member_id table_id variable_id grid_label version'.split(),
-    'cordex': 'project product domain institute driving_model experiment ensemble rcm_name rcm_version time_frequency variable'.split(),
-    'c3s-cmip5': 'activity product institute model experiment frequency realm mip_table ensemble_member variable version'.split(),
-    'c3s-cmip6': 'mip_era activity_id institution_id source_id experiment_id member_id table_id variable_id grid_label version'.split(),
-    'c3s-cordex': 'project product domain institute driving_model experiment ensemble rcm_name rcm_version time_frequency variable'.split()
+    "cmip5": "activity product institute model experiment frequency realm mip_table ensemble_member version variable".split(),
+    "cmip6": "mip_era activity_id institution_id source_id experiment_id member_id table_id variable_id grid_label version".split(),
+    "cordex": "project product domain institute driving_model experiment ensemble rcm_name rcm_version time_frequency variable".split(),
+    "c3s-cmip5": "activity product institute model experiment frequency realm mip_table ensemble_member variable version".split(),
+    "c3s-cmip6": "mip_era activity_id institution_id source_id experiment_id member_id table_id variable_id grid_label version".split(),
+    "c3s-cordex": "project product domain institute driving_model experiment ensemble rcm_name rcm_version time_frequency variable".split(),
 }
-mappings = {
-    'cmip6': {
-        'variable': 'variable_id'
-    }
-}
+mappings = {"cmip6": {"variable": "variable_id"}}
 
 
 def map_facet(facet, project):
--- tests/test_utils/test_json_store.py
+++ tests/test_utils/test_json_store.py
@@ -1,8 +1,8 @@
 # Tests for default JsonStore class
 import os
 import shutil
-import pytest
 
+import pytest
 
 from dachar.utils.json_store import _BaseJsonStore
 
@@ -10,19 +10,21 @@ from dachar.utils.json_store import _BaseJsonStore
 # Create a new dummy store to run tests on
 class _TestStore(_BaseJsonStore):
 
-    store_name = 'TestStore'
-    config = {'store_type': 'local',
-              'local.base_dir': '/tmp/test-store',
-              'local.dir_grouping_level': 4}
-    id_mappers = {'*': '__all__'}
-    required_fields = ['data']
-    search_defaults = ['id', 'data']
+    store_name = "TestStore"
+    config = {
+        "store_type": "local",
+        "local.base_dir": "/tmp/test-store",
+        "local.dir_grouping_level": 4,
+    }
+    id_mappers = {"*": "__all__"}
+    required_fields = ["data"]
+    search_defaults = ["id", "data"]
 
 
 recs = [
-    ('1.2.3.4.5.6.b', {'data': 'great match'}),
-    ('1.2.3.4.5.6.a', {'x': 'does not save'}),
-    ('2.2.2.2.2.2.c', {'data': {'d2': {'test': 123, 'test2': 'hi'}}})
+    ("1.2.3.4.5.6.b", {"data": "great match"}),
+    ("1.2.3.4.5.6.a", {"x": "does not save"}),
+    ("2.2.2.2.2.2.c", {"data": {"d2": {"test": 123, "test2": "hi"}}}),
 ]
 
 store = None
@@ -50,45 +52,45 @@ def test_put():
 
 
 def test_put_maps_asterisk():
-    _id = '1.1.*.1.*.*.1'
-    store.put(_id, {'data': 'test'})
+    _id = "1.1.*.1.*.*.1"
+    store.put(_id, {"data": "test"})
     fpath = store._id_to_path(_id)
 
-    bdir = store.config['local.base_dir']
-    assert(fpath == os.path.join(bdir, '1/1/__all__/1.__all__.__all__.1.json'))
-    assert(os.path.isfile(fpath))
+    bdir = store.config["local.base_dir"]
+    assert fpath == os.path.join(bdir, "1/1/__all__/1.__all__.__all__.1.json")
+    assert os.path.isfile(fpath)
     store.delete(_id)
 
 
 def test_get():
     rec = store.get(recs[0][0])
-    assert(rec == recs[0][1])
+    assert rec == recs[0][1]
 
 
 def test_delete():
     _id = recs[0][0]
     store.delete(_id)
-    assert(store.get(_id) is None)
+    assert store.get(_id) is None
 
     # Check that directories also get pruned
     dr = os.path.dirname(store._id_to_path(_id))
-    while len(dr) > len(store.config['local.base_dir']):
-        assert(not os.path.exists(dr))
+    while len(dr) > len(store.config["local.base_dir"]):
+        assert not os.path.exists(dr)
         dr = os.path.dirname(dr)
 
 
 def test_validate_non_json():
     try:
-        store._validate('rubbish')
+        store._validate("rubbish")
     except Exception as exc:
-        assert(str(exc) == 'Cannot serialise content to valid JSON.')
+        assert str(exc) == "Cannot serialise content to valid JSON."
 
 
 def test_put_fail_validate():
     try:
         store.put(*recs[1])
     except ValueError as exc:
-        assert(str(exc).find('Required content "data" not found.') > -1)
+        assert str(exc).find('Required content "data" not found.') > -1
 
 
 @pytest.mark.xfail(reason="tox test fails")
@@ -96,60 +98,66 @@ def test_get_all_ids():
     store.put(*recs[0])
     store.put(*recs[2])
     all_ids = [_ for _ in store._get_all_ids()]
-    assert(all_ids == [recs[0][0], recs[2][0]])
+    assert all_ids == [recs[0][0], recs[2][0]]
 
 
 @pytest.mark.xfail(reason="tox test fails")
 def test_search_by_term():
     # Search with default fields + exact match
-    resp = store.search('great match', exact=True, fields=None, ignore_defaults=False)
-    assert(resp == [recs[0][1]])
+    resp = store.search("great match", exact=True, fields=None, ignore_defaults=False)
+    assert resp == [recs[0][1]]
 
     # Search with custom fields + exact match
-    resp = store.search('great match', exact=True, fields=['data'], ignore_defaults=True)
-    assert(resp == [recs[0][1]])
+    resp = store.search(
+        "great match", exact=True, fields=["data"], ignore_defaults=True
+    )
+    assert resp == [recs[0][1]]
 
     # Search with default fields + partial match
-    resp = store.search('at MAT', exact=False, fields=None, ignore_defaults=False)
-    assert(resp == [recs[0][1]])
+    resp = store.search("at MAT", exact=False, fields=None, ignore_defaults=False)
+    assert resp == [recs[0][1]]
 
     # Search with custom nested fields + exact match
-    resp = store.search('123', exact=True, fields=['data.d2.test'], ignore_defaults=True)
-    assert(resp == [recs[2][1]])
+    resp = store.search(
+        "123", exact=True, fields=["data.d2.test"], ignore_defaults=True
+    )
+    assert resp == [recs[2][1]]
 
     # Search with default fields + partial match
-    resp = store.search('123', exact=False, fields=None, ignore_defaults=False)
-    assert(resp == [recs[2][1]])
+    resp = store.search("123", exact=False, fields=None, ignore_defaults=False)
+    assert resp == [recs[2][1]]
 
     # Search with custom nested fields + exact match as integer
-    resp = store.search(123, exact=True, fields=['data.d2.test'], ignore_defaults=True)
-    assert(resp == [recs[2][1]])
+    resp = store.search(123, exact=True, fields=["data.d2.test"], ignore_defaults=True)
+    assert resp == [recs[2][1]]
 
     # Search with custom multiple fields and partial match
-    resp = store.search('e', exact=False, fields=['data', 'data.d2.test'], ignore_defaults=True)
-    assert(resp == [recs[0][1], recs[2][1]])
+    resp = store.search(
+        "e", exact=False, fields=["data", "data.d2.test"], ignore_defaults=True
+    )
+    assert resp == [recs[0][1], recs[2][1]]
 
     # Search wih failed match
-    resp = store.search('zzz', exact=False, fields=None, ignore_defaults=False)
-    assert(resp == [])
+    resp = store.search("zzz", exact=False, fields=None, ignore_defaults=False)
+    assert resp == []
 
 
 @pytest.mark.xfail(reason="tox test fails")
 def test_search_by_id():
     # Search for non-existent id
-    resp = store.search('zzz', exact=False, match_ids=True, ignore_defaults=True)
-    assert(resp == [])
+    resp = store.search("zzz", exact=False, match_ids=True, ignore_defaults=True)
+    assert resp == []
 
     # Search by partial ID
-    resp = store.search('5.6.b', exact=False, match_ids=True, ignore_defaults=True)
-    assert(resp == [recs[0][1]])
+    resp = store.search("5.6.b", exact=False, match_ids=True, ignore_defaults=True)
+    assert resp == [recs[0][1]]
 
     # Search for exact ID
     resp = store.search(recs[0][0], exact=True, match_ids=True, ignore_defaults=False)
-    assert(resp == [recs[0][1]])
+    assert resp == [recs[0][1]]
 
 
 def teardown_module():
-    dr = _TestStore.config['local.base_dir']
+    dr = _TestStore.config["local.base_dir"]
     if os.path.isdir(dr):
         shutil.rmtree(dr)
