d55a03afffc4399eda34e00e4d61d74dc0e2d234
Eleanor Smith <esmith88@jasmin-sci5.ceda.ac.uk>
2020-08-05 12:33:00 +0100

    incorporating roocs-utils

--- dachar/utils/character.py
+++ dachar/utils/character.py
@@ -3,66 +3,7 @@ from datetime import datetime
 import numpy as np
 import xarray as xr
 from cfunits import Units
-
-
-# NOTE THESE ARE COMMON WITH clisops - need to merge!!!
-def get_coord_by_attr(dset, attr, value):
-    coords = dset.coords
-
-    for coord in coords.values():
-        if coord.attrs.get(attr, None) == value:
-            return coord
-
-    return None
-
-
-def is_latitude(coord):
-    if hasattr(coord, "units"):
-        if Units(coord.units).islatitude:
-            return True
-
-    elif coord.attrs.get("standard_name", None) == "latitude":
-        return True
-
-
-def is_longitude(coord):
-    if hasattr(coord, "units"):
-        if Units(coord.units).islongitude:
-            return True
-
-    elif coord.attrs.get("standard_name", None) == "longitude":
-        return True
-
-
-def is_level(coord):
-    raise NotImplementedError()
-
-
-def is_time(coord):
-
-    if coord.values.size > 1:
-        if hasattr(coord.values[0], "calendar"):
-            if Units(calendar=coord.values[0].calendar).isreftime:
-                return True
-
-    elif hasattr(coord, "axis"):
-        if coord.axis == "T":
-            return True
-
-    elif coord.attrs.get("standard_name", None) == "time":
-        return True
-
-
-def get_coord_type(coord):
-
-    if is_longitude(coord):
-        return "longitude"
-    elif is_latitude(coord):
-        return "latitude"
-    elif is_time(coord):
-        return "time"
-
-    return None
+from roocs_utils import xarary_utils
 
 
 def get_coords(da):
@@ -88,7 +29,7 @@ def get_coords(da):
 
         coord = da.coords[coord_id]
 
-        coord_type = get_coord_type(coord)
+        coord_type = xarary_utils.get_coord_type(coord)
         name = coord_type or coord.name
         data = coord.values
 
