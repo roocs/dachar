eb2539cae2baa76b2c99fd47889e3f855eb26341
Eleanor Smith <esmith88@jasmin-sci5.ceda.ac.uk>
2020-08-05 12:33:00 +0100

    incorporating roocs-utils

---
+++ tests/test_utils/test_options.py
@@ -0,0 +1,28 @@
+import os
+import mock
+from importlib import reload
+from dachar.utils import options
+
+
+def test_project_dirs_no_env_var():
+    assert options.project_base_dirs["cmip5"] == "/badc/cmip5/data"
+    assert options.project_base_dirs["cmip6"] == "/badc/cmip6/data"
+    assert options.project_base_dirs["cordex"] == "/badc/cordex/data"
+    assert options.project_base_dirs["c3s-cmip5"] == "/group_workspaces/jasmin2/cp4cds1/vol1/data/"
+    assert options.project_base_dirs["c3s-cmip6"] == "NOT DEFINED YET"
+    assert options.project_base_dirs["c3s-cordex"] == "/group_workspaces/jasmin2/cp4cds1/vol1/data/"
+
+
+@mock.patch.dict(os.environ, {"ROOCS_CONFIG": "/home/users/esmith88/roocs/"})
+def test_project_dirs_paths_env_var():
+    reload(options)
+
+    assert options.project_base_dirs["cmip5"] == "cmip5/file/path/for/tests"
+    assert options.project_base_dirs["cmip6"] == "cmip6/file/path/for/tests"
+    assert options.project_base_dirs["cordex"] == "cordex/file/path/for/tests"
+    assert options.project_base_dirs["c3s-cmip5"] == "c3s-cmip5/file/path/for/tests"
+    assert options.project_base_dirs["c3s-cmip6"] == "c3s-cmip6/file/path/for/tests"
+    assert options.project_base_dirs["c3s-cordex"] == "c3s-cordex/file/path/for/tests"
+
+
+
