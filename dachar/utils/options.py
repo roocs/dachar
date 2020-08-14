154776c104a84e05e5f9bb0fc738108e7965f739
Eleanor Smith <esmith88@jasmin-sci5.ceda.ac.uk>
2020-08-14 15:36:46 +0100

    work on getting config project base dirs

--- dachar/utils/options.py
+++ dachar/utils/options.py
@@ -1,16 +1,18 @@
 import os
 import sys
 
+from roocs_utils import config 
+
+project_base_dirs = config.project_base_dirs
+
 if "ROOCS_CONFIG" in os.environ:
     config_path = os.environ["ROOCS_CONFIG"]
     sys.path.insert(1, config_path)
     import config_local as config
-
-else:
-    import roocs_utils.config as config
-
-project_base_dirs = config.project_base_dirs
-
+    # project_base_dirs = {k: config.project_base_dirs[k] for k, _ in set(set(config.project_base_dirs.items() - project_base_dirs.items()))}
+    project_base_dirs = dict(set(config.project_base_dirs.items() ^ set(project_base_dirs.items())))
+    # project_base_dirs = dict(set(project_base_dirs.items() ^ set(config.project_base_dirs.items())))
+    print(project_base_dirs)
 
 def get_project_base_dir(project):
     return project_base_dirs.get(project)
